4133e1d116f83ba156f802465f900a7ea784e236
LunarHunter <coolcoolvideos@gmail.com>
2020-03-25 10:58:54 +0000

    v1.01

--- BruteForceAltF4/main.cpp
+++ BruteForceAltF4/main.cpp
@@ -4,59 +4,39 @@
 #include <fstream>
 #include <psapi.h>
 
-void HideConsole()
-{
-	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-}
-
-void ShowConsole()
-{
-	::ShowWindow(::GetConsoleWindow(), SW_SHOW);
-}
 using namespace std;
-
+void HideConsole() { ::ShowWindow(::GetConsoleWindow(), SW_HIDE); }
+void ShowConsole() { ::ShowWindow(::GetConsoleWindow(), SW_SHOW); }
+UINT DefaultMsgBoxTypes = MB_ICONERROR | MB_OK | MB_DEFBUTTON1;
 void killFocusedWindow() {
 	DWORD processID;
-	HWND hwnd;
-	hwnd = GetForegroundWindow();
+	HWND hwnd = GetForegroundWindow();
 	cout << GetWindowThreadProcessId(hwnd, &processID) << "\n";
-	
-	//This code here was to get the window title but quickly discovered that it was useless. It still works though, so if you want to add it, just simply uncomment it.
-
-	//string data; // The data the user has typed will be stored here
-	//data.resize(GetWindowTextLength(hwnd) + 1, '\0'); // resize the string so iit can contain the text stored in the edit-control.
-
-	//GetWindowText(hwnd, LPSTR(data.c_str()), GetWindowTextLength(hwnd) + 1); // Getting the data the user typed
-	//cout << data << "\n";
 
 	cout << "Got process ID: " << processID << "\n";
-	HANDLE process1;
-	process1 = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
+	HANDLE process1 = OpenProcess(PROCESS_ALL_ACCESS, false, processID);;
 
 	TCHAR nameProc[MAX_PATH];
 	
 	GetProcessImageFileName(process1, nameProc, MAX_PATH);
 	cout << nameProc <<"\n";
 	string NameProc1 = nameProc;
-	if (NameProc1.find("\\explorer.exe") != string::npos ) { //or NameProc1.find("\\SearchUI.exe") != string::npos
-		cout << "Program cannot be terminated! In the exclusions list\n";
-		return;
-	}
-	int errorcode = 1;
-	errorcode = TerminateProcess(process1, 1);
+
+	//or NameProc1.find("\\SearchUI.exe") != string::npos
+	if (NameProc1.find("\\explorer.exe") != string::npos ) { cout << "Program cannot be terminated! In the exclusions list\n";return;}
+
+	int errorcode = TerminateProcess(process1, 1);
 	CloseHandle(process1);
 	NameProc1.clear();
 	cout << errorcode << "BRU\n";
-	if (errorcode == 1) {
-		cout << "Terminated Process By ID\n";
-	}
-	if (errorcode == 0) {
+	if (errorcode == 1) {cout << "Terminated Process By ID\n";}
+	else if (errorcode == 0) {
 		cout << "Termination Failed! Elevated Permissions Could Be Required!\n";
 		MessageBox(
 			NULL,
 			"Termination Failed! Elevated Permissions Could Be Required!",
 			"Termination Failure - BruteForceAltF4",
-			MB_ICONERROR | MB_OK | MB_DEFBUTTON1
+			DefaultMsgBoxTypes
 		);
 	}
 	return;
@@ -66,14 +46,8 @@ bool FileExist(string path)
 {
 	ifstream file(path);
 	if (file.is_open())
-	{
-		return 1; // Yes it exists
-		file.close();
-	}
-	else
-	{
-		return 0; // No it does not
-	}
+	{return 1;file.close();}
+	else{return 0;}
 }
 
 int main() {
@@ -81,7 +55,6 @@ int main() {
 	HideConsole();
 	//ShowConsole();
 	cout << "LunarHunter Brute Force Alt F4 - 2020 Debug Console\n";{}
-
 	//check if settings file exists here, show tos if not there
 	if (!FileExist("settings.ini")) {
 		cout << "Settings does not exist!\n";
@@ -96,23 +69,19 @@ int main() {
 			ofstream MyFile("settings.ini");
 			MyFile.close();
 		}
-		else {
-			return 0;
-		}
+		else {return 0;}
 
 	}
 	else {
 		//Read if the settings.ini file has debug mode enabled, if so, show the console.
 	}
-	if (RegisterHotKey(NULL, 1, MOD_ALT | MOD_CONTROL | MOD_NOREPEAT, 0x73) and RegisterHotKey(NULL, 2, MOD_ALT | MOD_CONTROL | MOD_SHIFT | MOD_NOREPEAT, 0x73)) {
-		cout << "Hotkeys Register Success!\n";
-	}
+	if (RegisterHotKey(NULL, 1, MOD_ALT | MOD_CONTROL | MOD_NOREPEAT, 0x73) and RegisterHotKey(NULL, 2, MOD_ALT | MOD_CONTROL | MOD_SHIFT | MOD_NOREPEAT, 0x73)) {cout << "Hotkeys Register Success!\n";}
 	else {
 		MessageBox(
 			NULL,
 			"Unable to register hot key! Program cannot continue.\nExiting...",
 			"Init Error - BruteForceAltF4",
-			MB_ICONERROR | MB_OK | MB_DEFBUTTON1
+			DefaultMsgBoxTypes
 		);
 		return 0;
 	}
@@ -128,12 +97,8 @@ int main() {
 				}
 				else if (msg.wParam == 2) {
 					cout << "hotkey id 2 recieved\n";
-					if (hidden) {
-						ShowConsole();
-					}
-					else {
-						HideConsole();
-					}
+					if (hidden) {ShowConsole();}
+					else {HideConsole();}
 					hidden = !hidden;
 					//settings window maybe?
 					//return 0;
@@ -143,3 +108,11 @@ int main() {
 		//killFocusedWindow();
 	}
 }
+
+//This code here was to get the window title but quickly discovered that it was useless. It still works though, so if you want to add it, just simply uncomment it.
+
+//string data; // The data the user has typed will be stored here
+//data.resize(GetWindowTextLength(hwnd) + 1, '\0'); // resize the string so iit can contain the text stored in the edit-control.
+
+//GetWindowText(hwnd, LPSTR(data.c_str()), GetWindowTextLength(hwnd) + 1); // Getting the data the user typed
+//cout << data << "\n";
